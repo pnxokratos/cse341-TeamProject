d4fcf8053dbb9c8085cd86aed9474e08d6b58d86
Helaman Rodrigues <u711602997@byui.edu>
2023-03-24 18:51:58 -0300

    swagger api-docs

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/travel', require('./travel'));
+router.use('/', require('./swagger'));
 
 module.exports = router;
